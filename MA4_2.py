5a36c241017eadae7cad685ca7ee7d7c0a53e458
Tyraaxen <tyra_axen@hotmail.se>
2023-10-10 22:01:46 +0200

    fgfgsvf

--- MA4_2.py
+++ MA4_2.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-
+from fib import fib_py
 from person import Person
 
 def main():
@@ -8,7 +8,8 @@ def main():
 	f.set(7)
 	print(f.get())
 	print('hej')
-	print(f.fib_cc())
+	print('cc: ', f.fib_cc())
+	print('python: ', fib_py(7))
 
 if __name__ == '__main__':
 	main()
