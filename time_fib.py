0c78f27ef902b94ad0a31159c10188af9671fde6
Tyraaxen <tyra_axen@hotmail.se>
2023-10-11 09:40:03 +0200

    done eveyrting

---
+++ time_fib.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+from fib import fib_py
+from  fib_numba import fib_numba
+from person import Person
+from time import perf_counter
+
+def main():
+    n = 20
+    t1_start = perf_counter()
+    f = Person(n)
+    f_cc = f.fib()
+    t1_stop = perf_counter()
+    
+    'python'
+    t2_start = perf_counter
+    f_py = fib_py(n)
+    t2_stop = perf_counter()
+    
+    'numba'
+    t3_start = perf_counter
+    f_py = fib_numba(n)
+    t3_stop = perf_counter()
+    
+    print('cc: ', f_cc)
+    print('time for cc = ', t1_start-t1_stop)
+    print('python: ', fib_py(n))
+    print('time for py = ', t2_start-t2_stop)
+    print('numba: ', fib_numba(n))
+    print('time for nu = ', t3_start-t3_stop)
+
+if __name__ == '__main__':
+	main()
