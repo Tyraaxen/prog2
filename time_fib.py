eda5879a6bc87111033eb4e7e26925a15980d751
Tyraaxen <tyra_axen@hotmail.se>
2023-10-11 10:11:46 +0200

    thh

--- time_fib.py
+++ time_fib.py
@@ -3,31 +3,40 @@ from fib import fib_py
 from  fib_numba import fib_numba
 from person import Person
 from time import perf_counter
+import matplotlib.pyplot as plt
+import numpy as np
 
 def main():
-    list = [30,35,40,45]
-    for n in list:
+    t = np.arrange(30,40,1)
+    timelst_cc = []
+    timelst_py = []
+    timelst_nu = []
+    for n in range(30,40):
         t1_start = perf_counter()
         f = Person(n)
         f_cc = f.fib()
         t1_stop = perf_counter()
+        timelst_cc.append(t1_stop-t1_start)
         
         'python'
         t2_start = perf_counter()
         f_py = fib_py(n)
         t2_stop = perf_counter()
+        timelst_py.append(t2_stop-t2_start)
         
         'numba'
         t3_start = perf_counter()
         f_py = fib_numba(n)
         t3_stop = perf_counter()
+        timelst_nu.append(t3_stop-t3_start)
         
-        print('cc: ', f_cc)
-        print('time for cc = ', t1_start-t1_stop)
-        print('python: ', fib_py(n))
-        print('time for py = ', t2_start-t2_stop)
-        print('numba: ', fib_numba(n))
-        print('time for nu = ', t3_start-t3_stop)
+        # print('cc: ', f_cc)
+        # print('time for cc = ', t1_stop-t1_start)
+        # print('python: ', fib_py(n))
+        # print('time for py = ', t2_stop-t2_start)
+        # print('numba: ', fib_numba(n))
+        # print('time for nu = ', t3_stop-t3_start)
+    plt.plot(t,timelst_cc,'r--',t,timelst_nu,'bs',t,timelst_py,'g^',)
 
 if __name__ == '__main__':
 	main()
