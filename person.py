49a7f63b8a75093bffe8896f78864b89c8d4c183
Tyraaxen <tyra_axen@hotmail.se>
2023-10-11 09:41:13 +0200

    fhgf

--- person.py
+++ person.py
@@ -10,8 +10,8 @@ class Person(object):
 		lib.Person_get.restype = ctypes.c_int
 		lib.Person_set.argtypes = [ctypes.c_void_p,ctypes.c_int]
 		lib.Person_delete.argtypes = [ctypes.c_void_p]
-		lib.Person_fib_cc.argtypes = [ctypes.c_void_p]
-		lib.Person_fib_cc.restypes = ctypes.c_int
+		lib.Person_fib.argtypes = [ctypes.c_void_p]
+		lib.Person_fib.restypes = ctypes.c_int
 		self.obj = lib.Person_new(age)
 
 	def get(self):
@@ -20,8 +20,8 @@ class Person(object):
 	def set(self, age):
 		lib.Person_set(self.obj, age)
         
-	def fib_cc(self):
-		return lib.Person_fib_cc(self.obj)
+	def fib(self):
+		return lib.Person_fib(self.obj)
         
 	def __del__(self):
 		return lib.Person_delete(self.obj)
